1f3393c98673544a9a7c6347f9ba2dc6941a1c38
PowerOleg <geraskin.oleg@mail.ru>
2022-12-20 17:47:53 +0500

    GeoServiceTest 1.5-snapshot

--- src/test/java/ru/netology/geo/GeoServiceTest.java
+++ src/test/java/ru/netology/geo/GeoServiceTest.java
@@ -1,34 +1,60 @@
 package ru.netology.geo;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
 import ru.netology.entity.Country;
 import ru.netology.entity.Location;
 
 public class GeoServiceTest {
+    GeoService geoService;
 
-    @ParameterizedTest
-    @ValueSource(strings = {})
-    public void testByIpPositive() {
-        String expected = "New York";
-        String expected2 = "Moscow";
-        Country expectedCountry = Country.RUSSIA;
-        Country expectedCountry2 = Country.USA;
-//        String expected3 = LOCALHOST;
-        Location expected4 = new Location(null, null, null, 0);
-        Location expected5 = new Location("Moscow", Country.RUSSIA, "Lenina", 15);
-        Location expected6 = new Location("New York", Country.USA, " 10th Avenue", 32);
-        Location expected7 = new Location("Moscow", Country.RUSSIA, null, 0);
-        Location expected8 = new Location("New York", Country.USA, null,  0);
-
-
-
-
-
-        
+    @BeforeEach
+    public void init() {
+        geoService = new GeoServiceImpl();
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"96.44.183.149", "96.0.0.0", "96.44.0.0", "96.44.255.255"})
+    public void testByIpUsaPositive(String argument) {
+        String expectedCity = "New York";
+        Country expectedCountry = Country.USA;
+//        Location expected = new Location("New York", Country.USA, null,  0);
 
+        Country resultCountry = geoService.byIp(argument).getCountry();
+        String resultCity = geoService.byIp(argument).getCity();
 
+        Assertions.assertEquals(expectedCountry, resultCountry);
+        Assertions.assertEquals(expectedCity, resultCity);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"172.0.32.11", "172.16.0.0", "172.31.255.255"})
+    public void testByIpRusPositive(String argument) {
+        String expectedCity = "Moscow";
+        Country expectedCountry = Country.RUSSIA;
+//        Location expected = new Location("Moscow", Country.RUSSIA, "Lenina", 15);
+
+        Country resultCountry = geoService.byIp(argument).getCountry();
+        String resultCity = geoService.byIp(argument).getCity();
+
+        Assertions.assertEquals(expectedCountry, resultCountry);
+        Assertions.assertEquals(expectedCity, resultCity);
+    }
+//
+//    @ValueSource(strings = {"127.0.0.1"})
+
+//        String expected3 = LOCALHOST;
+//Location expected4 = new Location(null, null, null, 0);
+
+//
+//    Location expected7 = new Location("Moscow", Country.RUSSIA, null, 0);
 }
+
+
+
+
+//        Location expected6 = new Location("New York", Country.USA, " 10th Avenue", 32);
